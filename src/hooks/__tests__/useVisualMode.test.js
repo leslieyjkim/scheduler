18eca1761aa069fbc8eed8154da58fabdffbffbc
yoonjookim <yjblossom@gmail.com>
2024-01-17 17:33:00 -0500

    Add Transition replacement with refactoring(adding condition/ex.error cases)

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -10,12 +10,15 @@ test("useVisualMode should initialize with default value", () => {
   expect(result.current.mode).toBe(FIRST);
 });
 
+//transition(move forward)
 test("useVisualMode should transition to another mode", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
   act(() => result.current.transition(SECOND));
   expect(result.current.mode).toBe(SECOND);
 });
 
+
+//back (move backward)
 test('useVisualMode returns to previous mode', () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
@@ -35,6 +38,21 @@ test('useVisualMode returns to previous mode', () => {
 //back limit : It should not allow the user to go back past the initial mode.(history array length is greater than or equal to 1)
 test('useVisualMode does not return to previous mode if already at initial mode', () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+
+//Transition with replace
+test('useVisualMode replaces the current mode', () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+  // Passing "true" to transition(THIRD, true) says "Transition to THIRD by REPLACING SECOND"
+  act(() => result.current.transition(THIRD, true));
+  expect(result.current.mode).toBe(THIRD);
 
   act(() => result.current.back());
   expect(result.current.mode).toBe(FIRST);
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,9 +3,16 @@ import React, { useState } from 'react';
 const useVisualMode = (initial) => {
   const [history, setHistory] = useState([initial]); //Implement a way to add a new mode to the history state array(initial array)
 
-  function transition(newMode) {
-    setHistory((prev) => [...prev, newMode]);
+
+  //In some situations we'll transition twice and then have the back action return us to the initial state. (예:저장 중 오류발생화면/ 뒤로 가면 status상태말고, 초기양식으로 표시)
+  function transition(newMode, replace = false) {
+    setHistory((prev) =>
+      replace
+        ? [...prev.slice(0, prev.length - 1), newMode]  //When replace is true then set the history to reflect that we are replacing the current mode.
+        : [...prev, newMode]
+    );
   }
+
 
   function back() {
     // setHistory((prev) => [...prev.slice(0, prev.length - 1)]); //because of 'back limit':prevent from going back past the initial mode
