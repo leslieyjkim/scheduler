099fd8101aa6e6c6bf699a8e3ddc32c86ee8d579
yoonjookim <yjblossom@gmail.com>
2024-01-18 18:10:03 -0500

    Install prop-types library & Add test in Appointment.test.js

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -5,11 +5,19 @@ import { render, cleanup, fireEvent } from "@testing-library/react";
 import Button from "components/Button";
 
 afterEach(cleanup);
-
+//This test verifies that we can render the component to the DOM without throwing an error.
+//declare the test using the it function provided by Jest. 
+//first argument is a descriptive name for the test, "render without crashing"
+//The second argument is a function that contains the test code
 it("renders without crashing", () => {
   render(<Button />);
 });
 
+
+//render function is imported from the react-testing-library.
+//expect function is injected into the global scope by Jest.
+//getByText query function is returned by the render function but is a part of the the dom-testing-library.
+//toBeInTheDocument function is a matcher provided through Jest by the jest-dom library.
 it.skip("renders its `children` prop as text", () => {
   const { getByText } = render(<Button>Default</Button>);
   expect(getByText("Default")).toBeInTheDocument();
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,2 @@
 import "@testing-library/react/cleanup-after-each";
-import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom/extend-expect";//The project boilerplate is configured to extend the default matchers with more specific matchers that help test the DOM. The src/setupTests.js file configures Jest to use jest-dom.
