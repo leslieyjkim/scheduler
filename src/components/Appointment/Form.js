e37cede61602e483b6646f11b3979f5d5c40006a
yoonjookim <yjblossom@gmail.com>
2024-01-18 21:05:40 -0500

    Add tests in Appointment Form Input with Submit button(test passed/but browser error)

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -25,6 +25,11 @@ export default function Form(props) {
       return;
     }
 
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+
     setError("");
     props.onSave(name, interviewer);
   }
@@ -42,6 +47,7 @@ export default function Form(props) {
             onChange={event => {
               setName(event.target.value);
             }}
+            data-testId='student-name-input' //add this line for Form.test.js line31:expect(getByTestId('student-name-input')).toHaveValue('Lydia Miller-Jones');
           />
         </form>
         <section className="appointment__validation">{error}</section>
