c01735c75f319617f2d21e9ae075a06ca856f50f
yoonjookim <yjblossom@gmail.com>
2024-01-19 22:02:32 -0500

    Add integration testing for cancelling appointment

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitForElement, fireEvent, prettyDOM, getByText, getByPlaceholderText, getByAltText, getAllByTestId, queryByText } from '@testing-library/react';
+import { render, waitForElement, fireEvent, prettyDOM, getByText, getByPlaceholderText, getByAltText, getAllByTestId, queryByText, queryByAltText } from '@testing-library/react';
 import Application from 'components/Application';
 
 describe('Application', () => {
@@ -33,7 +33,7 @@ describe('Application', () => {
     fireEvent.click(getByText(appointment, 'Save'));
     // console.log(prettyDOM(appointment));
     // debug();
-    
+
     expect(getByText(appointment, 'Saving')).toBeInTheDocument();
 
     await waitForElement(() => getByText(appointment, 'Lydia Miller-Jones'));
@@ -45,6 +45,34 @@ describe('Application', () => {
     expect(getByText(day, 'no spots remaining')).toBeInTheDocument();
 
     // console.log(prettyDOM(day));
+  });
+
+  it('loads data, cancels an interview and increases the spots remaining for Monday by 1', async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, 'Archie Cohen'));
+
+    const appointment = getAllByTestId(container, 'appointment').find(
+      (appointment) => queryByText(appointment, 'Archie Cohen')
+    );
+
+    fireEvent.click(queryByAltText(appointment, 'Delete'));
+
+    expect(
+      getByText(appointment, 'Are you sure you would like to delete?')
+    ).toBeInTheDocument();
+
+    fireEvent.click(queryByText(appointment, 'Confirm'));
+
+    expect(getByText(appointment, 'Deleting')).toBeInTheDocument();
+
+    await waitForElement(() => getByAltText(appointment, 'Add'));
+
+    const day = getAllByTestId(container, 'day').find((day) =>
+      queryByText(day, 'Monday')
+    );
+
+    expect(getByText(day, '2 spots remaining')).toBeInTheDocument();
   });
 });
 //switching Promise to using async and await.(not replacing, work well together)
